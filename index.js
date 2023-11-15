b3684be5ba8e0c1a061859ee7edd4c6cc8e5f3bc
ThongZen <pascalthong@gmail.com>
2023-11-15 20:55:07 +0000

    test codespace

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
+
+import express from 'express'
 const app = express()
 const port = 3000
 
+
+console.log("app is started");
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json([
+    {projectName: "Hoger Lager", description: "Maak een het spel Hoger Lager"},
+    {projectName: "Build A Game", description: "Ontwikkel een spel"}
+  ]);
 })
 
 app.listen(port, () => {
+  console.log('api should be working')
   console.log(`Example app listening on port ${port}`)
 })
